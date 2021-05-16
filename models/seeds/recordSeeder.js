47e470542f337525370e9ead50a46ae8d9f9b811
Debbie <debbiecamera0704@gmail.com>
2021-05-16 18:33:33 +0800

    modify seeds

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,20 +1,14 @@
-const Record = require('../Record')
-const moment = require('moment')
+const Record = require('../Records')
+const recordList = require('./record.json')
 const db = require('../../config/mongoose')
 
-const category = ['home', 'utensils', 'pen', 'laugh-squint', 'shuttle-van']
-
 db.once('open', () => {
-  for (i = 0; i < 11; i++) {
-    let randomCategory = Math.floor(Math.random() * 5)
-    let randomAmount = Math.floor(Math.random() * 501)
-    Record.create({
-      name: '測試' + i,
-      date: '01.02.2012',
-      category: category[randomCategory],
-      amount: randomAmount
+  Record.create(recordList)
+    .then(() => {
+      console.log('Seeds are created!')
+      return db.close()
     })
-  }
-  console.log('Seeds are created!')
+    .then(() => {
+      console.log('database connection close...')
+    })
 })
-
