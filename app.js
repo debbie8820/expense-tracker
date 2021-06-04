e03efd7327e7c83988540b4721927d7f7cb7f590
Debbie <debbiecamera0704@gmail.com>
2021-06-04 16:24:02 +0800

    feat: add res.locals

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated
+  res.locals.user = req.user
+  next()
+})
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(routes)
