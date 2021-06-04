bdcd50e844ea7766ae50be4025719ec2a9b66c7b
Debbie <debbiecamera0704@gmail.com>
2021-06-04 15:46:48 +0800

    feat: add user authenticator

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const home = require('./modules/home')
 const record = require('./modules/record')
 const filter = require('./modules/filter')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 
 router.use('/users', users)
-router.use('/records', record)
-router.use('/filter', filter)
-router.use('/', home)
+router.use('/records', authenticator, record)
+router.use('/filter', authenticator, filter)
+router.use('/', authenticator, home)
 
 module.exports = router
